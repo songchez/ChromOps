4015e988594a29957c33d5bf726d6036c37520d6
songchez <tama4840@gmail.com>
2024-09-10 17:21:43 +0900

    shop파일정리

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -1,10 +1,27 @@
-import ProductGrid from "@/components/shop/ProductGrid";
-import products from "@/data/products.json";
-
+import Image from "next/image";
+import products from "../../data/products.json";
 export default function ShopPage() {
   return (
-    <div className="p-6">
-      <ProductGrid products={products} />
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className="bg-gray-800 rounded-sm overflow-hidden"
+        >
+          <Image
+            src={`/products/${product.id}/thumbnail_1.jpg`}
+            alt={product.name}
+            width={300}
+            height={250}
+            layout="responsive"
+          />
+          <div className="p-4">
+            <h3 className="text-xl font-bold mb-2">{product.name}</h3>
+            <p className="mb-4">{product.description}</p>
+            <p> {product.price}₩</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
