0331cc933d8baadf824daea50a8eeb52d455487f
songchez <tama4840@gmail.com>
2024-09-17 09:56:44 +0900

    font

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -6,17 +6,17 @@ export default async function BlogPostPage({ params }) {
   );
 
   const typoTheme = `max-w-3xl p-3 prose prose-strong:text-blue-400 prose-hr:border-gray-300
-    dark:prose-invert
-    dark:prose-headings:text-white
-    dark:prose-p:text-white
-    dark:prose-h1:text-4xl
-    dark:prose-h1:text-blue-500
-    dark:prose-h1:decoration-fuchsia-600
-    dark:prose-h2:text-3xl
-    dark:prose-h3:text-2xl
-    dark:prose-h4:text-xl
-    dark:prose-h5:text-lg
-    dark:prose-h6:text-lg
+    prose-invert
+    prose-headings:text-white
+    prose-p:text-white
+    prose-h1:text-4xl
+    prose-h1:text-blue-500
+    prose-h1:decoration-fuchsia-600
+    prose-h2:text-3xl
+    prose-h3:text-2xl
+    prose-h4:text-xl
+    prose-h5:text-lg
+    prose-h6:text-lg
   `;
 
   return (
