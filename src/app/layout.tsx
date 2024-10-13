42c789e358e2279e1c5ef1b0671261c8189fdef7
songchez <tama4840@gmail.com>
2024-10-13 22:03:55 +0900

    구글 소유권증명

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,10 @@ const notoSans = Noto_Sans_KR({ weight: ["400"], subsets: ["latin"] });
 
 export const metadata = {
   title: "ChromOps",
-  description: "역전, 승리, 크로몹스",
+  description: "역전, 승리, 크로몹스. 승리를 이끄는 패션브랜드스토어",
+  verification: {
+    google: "D1aDELtUBHP7GQB0FUQ4GyRvpZOU4qZfLqD11pl2qUM",
+  },
 };
 
 export default function RootLayout({
